8d2551586c0fe88c78fa039c385cc44ba2eff0ba
Sylvain BONNEFON <sylvainbonnefon@hotmail.com>
2020-04-23 15:09:04 +0200

    normalise to pass pylama linter

--- D00/ex01/exec.py
+++ D00/ex01/exec.py
@@ -2,4 +2,9 @@
 import sys
 
 if __name__ == '__main__':
-    print(''.join(c.lower() if c.isupper() else c.upper() for c in ' '.join(sys.argv[1:]).strip()[::-1]))
+    print(
+        ''.join(c.lower()
+                if c.isupper()
+                else c.upper()
+                for c in ' '.join(sys.argv[1:]).strip()[::-1])
+    )
