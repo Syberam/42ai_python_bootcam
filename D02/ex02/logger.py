fb271b31c27365c009af4ed966fdcf13b7d54b1d
Sylvain BONNEFON <sylvainbonnefon@hotmail.com>
2020-04-25 13:07:19 +0200

    logger decorator print on standar output

---
+++ D02/ex02/logger.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import time
+import getpass
+from random import randint
+
+
+def log(function):
+    def wrapper(*args, **kwargs):
+        t_in = time.time()
+        ret = function(*args, **kwargs)
+        t_out = time.time()
+        exec_time = t_out - t_in
+        if exec_time > 0.001:
+            exec_time_str = "{:.3f} s ".format(exec_time)
+        else:
+            exec_time_str = "{:.3f} ms".format(exec_time * 1000)
+        print("({})Running: {:<20} [ exec-time = {} ]".format(
+            getpass.getuser(),
+            function.__name__,
+            exec_time_str
+        ))
+        return ret
+    return wrapper
+
+
+class CoffeeMachine():
+    water_level = 100
+    @log
+    def start_machine(self):
+        if self.water_level > 20:
+            return True
+        else:
+            print("Please add water!")
+            return False
+
+    @log
+    def boil_water(self):
+        return "boiling..."
+
+    @log
+    def make_coffee(self):
+        if self.start_machine():
+            for _ in range(20):
+                time.sleep(0.1)
+                self.water_level -= 1
+            print(self.boil_water())
+            print("Coffee is ready!")
+
+    @log
+    def add_water(self, water_level):
+        time.sleep(randint(1, 5))
+        self.water_level += water_level
+        print("Blub blub blub...")
+
+
+if __name__ == "__main__":
+    machine = CoffeeMachine()
+    for i in range(0, 5):
+        machine.make_coffee()
+
+    machine.make_coffee()
+    machine.add_water(70)
